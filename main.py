8e7946360c555ba4c08b429e29c4737447cb411e
Eduardo Alejandro González Martell <eduardoglez64377@gmail.com>
2025-12-18 12:36:44 -0500

    sync

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
+from datetime import datetime
+
 from core.analyzer import analyze_data
-from dotenv import load_dotenv
-import os
 from core.scrapper import process_latest_messages, process_all_messages
 
 if __name__ == '__main__':
-    # LOAD ENVS
-    load_dotenv()
-    year = int(os.getenv("YEAR"))
+    first_year = 2022
+    years = list(range(first_year, datetime.now().year + 1))
 
     # Message Retrieving from Telegram
     # process_all_messages()
-    # process_latest_messages()
+    process_latest_messages()
 
     # Data Analysis
-    analyze_data(year)
+    for year in years:
+        analyze_data(year)
