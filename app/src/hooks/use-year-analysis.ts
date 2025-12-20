dae1a6e024d9e3c6b04ed327a8d56e74c86f66de
Eduardo Alejandro González Martell <eduardoglez64377@gmail.com>
2025-12-20 10:26:48 -0500

    Revert "feat: cache implementation"
    This reverts commit 03c31e1a1c71fc9b66449036a00739c18b532fd4.

--- app/src/hooks/use-year-analysis.ts
+++ app/src/hooks/use-year-analysis.ts
@@ -1,44 +1,29 @@
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useState} from "react";
 import {UneAnalysis} from "@/src/common/types.ts";
 
 export default function useYearAnalysis(selectedYear: number) {
     const [data, setData] = useState<UneAnalysis | null>(null)
     const [loading, setLoading] = useState(false)
-    const cache = useRef<Record<number, UneAnalysis>>({});
 
     useEffect(() => {
-        const controller = new AbortController();
+        const controller = new AbortController()
 
-        const loadData = async () => {
-            if (cache.current[selectedYear]) {
-                setLoading(true);
-                // Just for the animation
-                await new Promise((resolve) => setTimeout(resolve, 1));
-                setData(cache.current[selectedYear]);
-                setLoading(false);
-                return;
-            }
+        setLoading(true)
+        fetch(`/data/analysis_data_${selectedYear}.json`, {
+            signal: controller.signal
+        })
+            .then(r => {
+                if (!r.ok) throw new Error()
+                return r.json()
+            })
+            .then(setData)
+            .catch(e => {
+                if (e.name !== 'AbortError') console.error(e)
+            })
+            .finally(() => setLoading(false))
 
-            setLoading(true);
-            try {
-                const response = await fetch(`/data/analysis_data_${selectedYear}.json`, {
-                    signal: controller.signal,
-                });
-                if (!response.ok) throw new Error();
-                const result = await response.json();
-                cache.current[selectedYear] = result;
-                setData(result);
-            } catch (e) {
-                if (e.name !== "AbortError") console.error(e);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        loadData();
-
-        return () => controller.abort();
-    }, [selectedYear]);
+        return () => controller.abort()
+    }, [selectedYear])
 
     return { data, loading }
 }
