bdac0f276be029c74e9c697581c01df4df49cb4c
Eduardo Alejandro González Martell <eduardoglez64377@gmail.com>
2025-12-22 20:55:27 -0500

    Revert "fix: pwa images"
    This reverts commit ffed70ba6a8f052cf730be4d69691b5a9f8201ce.

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -36,54 +36,21 @@ export default defineConfig(({ mode }) => {
                     theme_color: '#f97316',
                     background_color: '#ffffff',
                     display: 'standalone',
-                    orientation: 'portrait',
                     scope: '/',
                     start_url: '/',
                     icons: [
-                         {
-                            src: 'pwa.png',
-                            sizes: '72x72',
-                            type: 'image/png',
-                            purpose: 'any maskable'
-                        },
-                         {
-                            src: 'pwa.png',
-                            sizes: '96x96',
-                            type: 'image/png',
-                            purpose: 'any maskable'
-                        },
-                         {
-                            src: 'pwa.png',
-                            sizes: '128x128',
-                            type: 'image/png',
-                            purpose: 'any maskable'
-                        },
                         {
-                            src: 'pwa.png',
-                            sizes: '144x144',
-                            type: 'image/png',
-                            purpose: 'any maskable'
-                        },
-                         {
-                            src: 'pwa.png',
-                            sizes: '152x152',
-                            type: 'image/png',
-                            purpose: 'any maskable'
-                        },
-                        {
-                            src: 'pwa.png',
+                            src: 'pwa-192x192.png',
                             sizes: '192x192',
-                            type: 'image/png',
-                            purpose: 'any maskable'
+                            type: 'image/png'
                         },
                         {
-                            src: 'pwa.png',
-                            sizes: '384x384',
-                            type: 'image/png',
-                            purpose: 'any maskable'
+                            src: 'pwa-512x512.png',
+                            sizes: '512x512',
+                            type: 'image/png'
                         },
                         {
-                            src: 'pwa.png',
+                            src: 'pwa-512x512.png',
                             sizes: '512x512',
                             type: 'image/png',
                             purpose: 'any maskable'
